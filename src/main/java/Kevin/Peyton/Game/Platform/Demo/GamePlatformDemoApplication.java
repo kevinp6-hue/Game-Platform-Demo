444a7f967b89d0b3c2f04c6c50631115b5f3dd49
kevinp6-hue <kevinp6@umbc.edu>
2026-03-23 14:58:50 -0400

    initial commit

---
+++ src/main/java/Kevin/Peyton/Game/Platform/Demo/GamePlatformDemoApplication.java
@@ -0,0 +1,13 @@
+package Kevin.Peyton.Game.Platform.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamePlatformDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamePlatformDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Kevin/Peyton/Game/Platform/Demo/GamePlatformDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Kevin.Peyton.Game.Platform.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamePlatformDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
